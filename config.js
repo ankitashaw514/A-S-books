85c3910d7e581baef979a1dfc1c13bece43c33fe
Shweta Shaw <shwetashaw597@gmail.com>
2021-02-01 18:56:03 +0530

    final1

--- config.js
+++ config.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
-    MONGODB_URL: process.env.MONGODB_URL || "mongodb://localhost/books",
+    MONGODB_URL:"mongodb+srv://Ankita:ankitashweta@as-books.xmzjs.mongodb.net/books?retryWrites=true&w=majority" ,
+    //process.env.MONGODB_URL || "mongodb://localhost/books",
     JWT_SECRET: process.env.JWT_SECRET || "somethingsecret"
 };
